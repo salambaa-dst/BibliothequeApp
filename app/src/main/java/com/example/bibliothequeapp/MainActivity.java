c878e71326927992e64932e8710af727c4ee4919
Selemba sall <sallselemba@gmail.com>
2026-05-23 12:58:46 +0000

    Lab 3 -Ecran acceuil et bouton Toast

--- app/src/main/java/com/example/bibliothequeapp/MainActivity.java
+++ app/src/main/java/com/example/bibliothequeapp/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.bibliothequeapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // Associe le layout XML à l'activité
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Récupération du bouton via son ID
+        Button btnVoirLivres = findViewById(R.id.btnVoirLivres);
+
+        // Ajout d'un listener au clic
+        btnVoirLivres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "Accès à la liste des livres",
+                        Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
